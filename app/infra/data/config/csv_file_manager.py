88df29e76f06d556af6d10b3cef0b1aa2751e91d
Andrew Malandrin <del_malandrin@hotmail.com>
2022-10-14 11:18:31 -0300

    feat: add csv file manager

---
+++ app/infra/data/config/csv_file_manager.py
@@ -0,0 +1,21 @@
+import csv
+from typing import Any, List
+
+class CSVFileManager:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def add_line_to_tsv_file(self, line: str) -> None:
+        with open(self.file_path, 'a+', encoding='utf-8') as opened_file:
+            opened_file.write(line)
+
+    def read_tsv_file(self) -> List:
+        print(f'Lendo arquivo TSV: {self.file_path}')
+        result = []
+        with open(self.file_path, 'r', encoding='utf-8') as opened_file:
+            file_read = csv.reader(opened_file, delimiter='\t')
+            for row in file_read:
+                result.append(row)
+        print('File extracted Data: ', result)
+        return result
+        
