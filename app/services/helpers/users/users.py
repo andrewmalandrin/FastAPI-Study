594a48930709970c2f2832eac07942ead6160db8
Andrew Malandrin <del_malandrin@hotmail.com>
2022-10-17 12:18:36 -0300

    fix: implement list validation to users helper

--- app/services/helpers/users/users.py
+++ app/services/helpers/users/users.py
@@ -5,15 +5,16 @@ def mount_users_data(file: List) -> UsersData:
     users = []
 
     for row in file:
-        if row[0].isnumeric():
-            user = {
-                'id': int(row[0]),
-                'name': row[1],
-                'weight': float(row[2]),
-                'carbo_kg': float(row[3]),
-                'fat_kg': float(row[4]),
-                'age': float(row[5])
-            }
-            users.append(user)
+        if len(row) > 0:
+            if row[0].isnumeric():
+                user = {
+                    'id': int(row[0]),
+                    'name': row[1],
+                    'weight': float(row[2]),
+                    'carbo_kg': float(row[3]),
+                    'fat_kg': float(row[4]),
+                    'age': float(row[5])
+                }
+                users.append(user)
 
     return users
