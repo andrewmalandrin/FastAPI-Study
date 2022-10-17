12f983e5b6dddf9e969d1641ca9a7f138f2ee018
Andrew Malandrin <del_malandrin@hotmail.com>
2022-10-17 12:17:57 -0300

    fix: implement list validation to products helper

--- app/services/helpers/products/products.py
+++ app/services/helpers/products/products.py
@@ -5,16 +5,17 @@ def mount_products_data(file: List) -> ProductsData:
     products = []
 
     for row in file:
-        if row[1].isnumeric():
-            product = {
-                'name': row[0],
-                'portion': int(row[1]),
-                'portion_unity':row[2],
-                'carbohidrates': float(row[3]),
-                'proteins': float(row[4]),
-                'fat': float(row[5]),
-                'saturated_fat': float(row[6])
-            }
-            products.append(product)
+        if len(row) > 0:
+            if row[1].isnumeric():
+                product = {
+                    'name': row[0],
+                    'portion': int(row[1]),
+                    'portion_unity':row[2],
+                    'carbohidrates': float(row[3]),
+                    'proteins': float(row[4]),
+                    'fat': float(row[5]),
+                    'saturated_fat': float(row[6])
+                }
+                products.append(product)
 
     return products
