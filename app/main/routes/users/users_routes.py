2943b491d8a91a4961279336e2b45c66afd96a0d
Andrew Malandrin <del_malandrin@hotmail.com>
2022-10-17 10:16:47 -0300

    feat: implement get user by id route

--- app/main/routes/users/users_routes.py
+++ app/main/routes/users/users_routes.py
@@ -1,14 +1,40 @@
 from http import HTTPStatus
-from typing import List, Optional
 from fastapi import Response
 
-from app.domain.usecases.users import GetUsersResponse
-from app.main.factories import get_users_factory
+from app.domain.usecases.users import GetUsersResponse, GetUserByIdResponse, GetUserByIdParams
+from app.main.factories import get_user_by_id_factory, get_users_factory
 from app.main.adapters import fastapi_adapter
 
 from main import app
 
 TAG = ['Users']
+
+
+@app.get(
+    '/users/{user_id}',
+    responses={
+        HTTPStatus.OK.value: {
+            'description' : 'Get user by ID',
+            'model' : GetUserByIdResponse
+        }
+    },
+    tags=TAG
+)
+def get_user_by_id(user_id: int, response: Response):
+    request = {
+        'body' : GetUserByIdParams(
+            id=user_id
+        ),
+        'headers' : None,
+        'query' : None
+    }
+    result = fastapi_adapter(request, get_user_by_id_factory())
+
+    response.status_code = result.status_code
+
+    if result.body:
+        return result.body
+    return response
 
 @app.get(
     '/users',
@@ -34,3 +60,5 @@ def get_users(response: Response):
     if result.body:
         return result.body
     return response
+
+
