df43c63f2d3998bc67d069179cb907f5cb0a49ca
Andrew Malandrin <del_malandrin@hotmail.com>
2022-10-17 17:54:58 -0300

    feat: add path enums

--- app/main/factories/update_user_factory.py
+++ app/main/factories/update_user_factory.py
@@ -1,10 +1,11 @@
 from app.services.usecases.users import UpdateUser
 from app.infra.data.repositories.users import UsersRepository
 from app.infra.data.config import CSVFileManager
+from app.services.enums import PathsEnum
 
 def update_user_factory():
     file_manager_instance = CSVFileManager(
-        'D:/users/pichau/devprojects/training/python/fastapi-study/app/infra/data/tsf/users.tsv'
+        PathsEnum.USERS_PATH
     )
     return UpdateUser(
         users_repository=UsersRepository(file_manager_instance=file_manager_instance)
