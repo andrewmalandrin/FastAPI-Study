284df6311d39050abfbf82de3481e33e247b9583
Andrew Malandrin <del_malandrin@hotmail.com>
2022-10-28 14:04:20 -0300

    refactor: remove unused imports

--- app/domain/usecases/diets/update_diet.py
+++ app/domain/usecases/diets/update_diet.py
@@ -1,10 +1,7 @@
 from abc import abstractmethod
 from pydantic import BaseModel
-from typing import List, Optional
 
-from app.services.enums import ActionsEnum
 from app.services.helpers.http import HttpResponse
-from app.services.helpers.string import convert_snake_to_camel
 
 from app.domain.usecases import Usecase
 
