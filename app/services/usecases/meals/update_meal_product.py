8831c4f2763cb3552d8c5f5fd43623eabd3d2efe
Andrew Malandrin <del_malandrin@hotmail.com>
2022-11-01 11:16:57 -0300

    feat: implement meal product not found into update meal product usecase

--- app/services/usecases/meals/update_meal_product.py
+++ app/services/usecases/meals/update_meal_product.py
@@ -1,6 +1,7 @@
 from app.domain.usecases import UpdateMealProductContract, UpdateMealProductParams, UpdateMealProductResponse
 from app.services.contracts import MealProductsRepositoryContract, UpdateMealProductFileParams
 from app.services.helpers.http import HttpResponse, HttpStatus
+from app.services.errors import MealProductNotFound
 
 class UpdateMealProduct(UpdateMealProductContract):
     def __init__(
@@ -10,13 +11,17 @@ class UpdateMealProduct(UpdateMealProductContract):
         self.meal_products_repository = meal_products_repository
 
     def execute(self, params: UpdateMealProductParams) -> HttpResponse[UpdateMealProductResponse]:
-        meal_product = self.meal_products_repository.update_meal_product(
-            UpdateMealProductFileParams(
-                id=params.id,
-                portion=params.portion
+        try:
+            meal_product = self.meal_products_repository.update_meal_product(
+                UpdateMealProductFileParams(
+                    id=params.id,
+                    portion=params.portion
+                )
             )
-        )
-
+        except MealProductNotFound:
+            return HttpStatus.not_found_404(
+                'Meal product not found'
+            )
 
         return HttpStatus.accepted_202(
             UpdateMealProductResponse(
